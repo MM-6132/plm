7c645a74a8220f99c018f0c2a081cf86a3b4b8bb
Krylio <lucas.bechoua@edu.devinci.fr>
2023-03-15 04:06:16 +0100

    Breadcrumbs added

--- frontend/src/containers/FindProduct.tsx
+++ frontend/src/containers/FindProduct.tsx
@@ -12,16 +12,28 @@ const columns = [
         hide: true
     },
     {
-        field: "firstName",
-        headerName: "First Name"
+        field: "Version",
+        headerName: "Version"
     },
     {
-        field: "lastName",
-        headerName: "Last Name"
+        field: "Product name",
+        headerName: "Product name"
     },
     {
-        field: "age",
-        headerName: "Age"
+        field: "Reference",
+        headerName: "Reference"
+    },
+    {
+        field: "Description",
+        headerName: "Description"
+    },
+    {
+        field: "Added on",
+        headerName: "Added on"
+    },
+    {
+        field: "Modified on",
+        headerName: "Modified on"
     }
 ];
 
@@ -29,33 +41,111 @@ const columns = [
 const rows = [
     {
         "id": 1,
-        "firstName": "First Name 1",
-        "lastName": "Last Name 1",
-        "age": 1
+        "Version" : 2,
+        "Product name": "Cylinder Head",
+        "Reference": "RE_00CH000",
+        "Description": "A component of an internal combustion engine that sits above the cylinders on top of the cylinder block.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
     },
     {
         "id": 2,
-        "firstName": "First Name 2",
-        "lastName": "Last Name 2",
-        "age": 2
+        "Version" : 2,
+        "Product name": "Modified cylinder Head",
+        "Reference": "RE_00CH010",
+        "Description": "A component of an internal combustion engine that sits above the cylinders on top of the cylinder blockwith a modification of 2 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
     },
     {
         "id": 3,
-        "firstName": "First Name 3",
-        "lastName": "Last Name 3",
-        "age": 3
+        "Version" : 2,
+        "Product name": "Modified cylinder Head",
+        "Reference": "RE_00CH020",
+        "Description": "A component of an internal combustion engine that sits above the cylinders on top of the cylinder block with a modification of 2 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
     },
     {
         "id": 4,
-        "firstName": "First Name 4",
-        "lastName": "Last Name 4",
-        "age": 4
+        "Version" : 2,
+        "Product name": "Modified cylinder Head",
+        "Reference": "RE_00CH030",
+        "Description": "A component of an internal combustion engine that sits above the cylinders on top of the cylinder block with a modification of 3 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
     },
     {
         "id": 5,
-        "firstName": "First Name 5",
-        "lastName": "Last Name 5",
-        "age": 5
+        "Version" : 2,
+        "Product name": "Modified cylinder Head",
+        "Reference": "RE_00CH040",
+        "Description": "A component of an internal combustion engine that sits above the cylinders on top of the cylinder block with a modification of 4 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
+    },
+    {
+        "id": 6,
+        "Version" : 2,
+        "Product name": "Modified cylinder Head",
+        "Reference": "RE_00CH050",
+        "Description": "A component of an internal combustion engine that sits above the cylinders on top of the cylinder block with a modification of 5 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
+    },
+    {
+        "id": 7,
+        "Version" : 2,
+        "Product name": "Head gasket",
+        "Reference": "RE_00HGO000",
+        "Description": "A gasket that sits between the engine block and cylinder head in an internal combustion engine.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
+    },
+    {
+        "id": 8,
+        "Version" : 2,
+        "Product name": "Modified head gasket",
+        "Reference": "RE_00HGO010",
+        "Description": "A gasket that sits between the engine block and cylinder head in an internal combustion engine with a modification of 1 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
+    },
+    {
+        "id": 9,
+        "Version" : 2,
+        "Product name": "Modified head gasket",
+        "Reference": "RE_00HGO020",
+        "Description": "A gasket that sits between the engine block and cylinder head in an internal combustion engine with a modification of 2 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
+    },
+    {
+        "id": 10,
+        "Version" : 2,
+        "Product name": "Modified head gasket",
+        "Reference": "RE_00HGO030",
+        "Description": "A gasket that sits between the engine block and cylinder head in an internal combustion engine with a modification of 3 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
+    },
+    {
+        "id": 11,
+        "Version" : 2,
+        "Product name": "Modified head gasket",
+        "Reference": "RE_00HGO040",
+        "Description": "A gasket that sits between the engine block and cylinder head in an internal combustion engine with a modification of 4 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
+    },
+    {
+        "id": 12,
+        "Version" : 4,
+        "Product name": "Modified head gasket",
+        "Reference": "RE_00HGO050",
+        "Description": "A gasket that sits between the engine block and cylinder head in an internal combustion engine with a modification of 5 millimeters.",
+        "Added on" : "2021-01-01",
+        "Modified on" : "2022-01-01"
     }
 ]
 export default function FindProduct() {
@@ -91,7 +181,7 @@ export default function FindProduct() {
                     color: "black",
                     boxShadow: "1px 1px 1px black",
                     margin: "1%",
-                    width: "45%",
+                    width: "75%",
                     borderRadius: "10px",
                 }}>
                     <div style={{height: 600, width: '100%'}}>
